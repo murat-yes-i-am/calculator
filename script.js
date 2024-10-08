f1bf0d633161d9cdd92da27c133a5cb83e812971
murat-yesimbietov <yesimbietovm@gmail.com>
2024-10-08 19:18:34 +0300

    feat: add operands and isDotUsed variables

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const numberPartButtons = document.querySelectorAll('.button.number-part');
 const operationButtons = document.querySelectorAll('.button.operation');
 const auxiliaryButtons = document.querySelectorAll('.button.auxiliary');
 const outputElement = document.querySelector('.button.output');
+
+let firstOperand = 0;
+let secondOperand;
+let isDotUsed = false;
