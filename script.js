c733d430e69349123308166a138205d910850b5b
murat-yesimbietov <yesimbietovm@gmail.com>
2024-10-08 19:16:16 +0300

    feat: add auxiliaryButtons and operationButtons variables

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
-const numberPartButtons = document.querySelectorAll('.number-part');
-const outputElement = document.querySelector('.output')
+const numberPartButtons = document.querySelectorAll('.button.number-part');
+const operationButtons = document.querySelectorAll('.button.operation');
+const auxiliaryButtons = document.querySelectorAll('.button.auxiliary');
+const outputElement = document.querySelector('.button.output');
