e4ed55de0f3096f93850bcd5d839c8f04aaeb469
murat-yesimbietov <yesimbietovm@gmail.com>
2024-10-08 19:30:12 +0300

    feat: add operate function

--- script.js
+++ script.js
@@ -6,8 +6,22 @@ const outputElement = document.querySelector('.button.output');
 let firstOperand = 0;
 let secondOperand;
 let isDotUsed = false;
+let operator;
 
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+
+const operate = (operand1, operator, operand2) => {
+  switch (operator) {
+    case '+':
+      return add(operand1, operand2);
+    case '-':
+      return subtract(operand1, operand2);
+    case '*':
+      return multiply(operand1, operand2);
+    case '/':
+      return divide(operand1, operand2);
+  }
+}
