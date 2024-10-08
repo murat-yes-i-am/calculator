9c14e775c0f11a7cafd0273588f647b894b25531
murat-yesimbietov <yesimbietovm@gmail.com>
2024-10-08 19:27:02 +0300

    feat: add basic math operation functions

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ const outputElement = document.querySelector('.button.output');
 let firstOperand = 0;
 let secondOperand;
 let isDotUsed = false;
+
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
