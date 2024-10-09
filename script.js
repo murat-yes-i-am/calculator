818090094ce42fd0b384e5e8c315cbb401c5afc2
murat-yesimbietov <yesimbietovm@gmail.com>
2024-10-09 11:09:33 +0300

    refactor: change operate

--- script.js
+++ script.js
@@ -20,21 +20,16 @@ let secondOperand = '';
 let isDotUsed = false;
 let operator = null;
 
-const add = (a, b) => a + b;
-const subtract = (a, b) => a - b;
-const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
-
 const operate = (operand1, operator, operand2) => {
   switch (operator) {
     case '+':
-      return add(operand1, operand2);
+      return operand1 + operand2;
     case '-':
-      return subtract(operand1, operand2);
+      return operand1 - operand2;
     case '*':
-      return multiply(operand1, operand2);
+      return operand1 * operand2;
     case '/':
-      return divide(operand1, operand2);
+      return operand1 / operand2;
   }
 }
 
@@ -88,7 +83,7 @@ const calculate = () => {
   if (firstOperand && operator && secondOperand) {
     const operand1 = Number(firstOperand);
     const operand2 = Number(secondOperand);
-    
+
     firstOperand = operate(operand1, operator, operand2);
     secondOperand = '';
     operator = null;
