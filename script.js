8a8fd9c4cc28f897a1451e9711d1989e8d69162d
murat-yesimbietov <yesimbietovm@gmail.com>
2024-10-09 08:31:41 +0300

    feat: add fullFillOperand

--- script.js
+++ script.js
@@ -40,5 +40,12 @@ const clear = () => {
   outputElement.textContent = firstOperand;
 }
 
+const fullFillOperand = (operand, digit) => {
+  if (operand !== 0) {
+    return operand + digit;
+  } else {
+    return digit !== '0' ? digit : operand;
+  }
+}
 
 auxiliaryButtons.clear.addEventListener('click', clear);
